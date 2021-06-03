c5053662e6eb145990bdc1bbc204629f14ea8264
Aldrei Glenn Nuqui <68434908+SuichiNagi@users.noreply.github.com>
2021-06-03 14:18:07 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+var leftValue = 450, topValue = 100, direction = 'top', step = 1;
+		
+    	function update(){
+			if(step == 1){
+				step = 2;
+			} else {
+				step = 1;
+			}
+    		document.getElementById("character").style.left = leftValue+"px";
+    		document.getElementById("character").style.top = topValue+"px";
+			document.getElementById("character").style.backgroundImage = "url('img/"+direction+step+".png')";
+    	}
+
+    	document.onkeydown = function(e){
+    		console.log(e);
+    		if(e.keyCode == 37 && leftValue > 0) { // LEFT
+    			leftValue = leftValue - 10;
+                direction = 'left';
+    		}
+    		else if (e.keyCode == 39 && leftValue < 500) { // RIGHT
+    			leftValue = leftValue + 10;  
+				direction = 'right';
+    		}
+    		else if (e.keyCode == 40 && topValue < 500) { // DOWN
+				topValue = topValue + 10;
+				direction = 'down';
+    		}
+            else if (e.keyCode == 38 && topValue > 0) { // TOP
+				topValue = topValue - 10;
+				direction = 'top';
+    		}
+    		update();
+    	}
